4ce9d96ceb0a8eb657f3ef0112835376609aaff8
陈育智 <1742971776@qq.com>
2020-02-17 09:57:07 +0800

    第一版本

---
+++ README.md
@@ -0,0 +1,4 @@
+santic
+======
+
+A Symfony project created on October 15, 2019, 1:41 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+use OpenApi\Annotations as OA ;
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     *
+     */
+    public function indexAction(Request $request)
+    {
+
+	    $swagger=\OpenApi\scan(dirname(dirname(__FILE__)));
+
+    }
+}
